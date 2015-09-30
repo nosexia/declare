5e60856a7fd7077cca4206a73134736309e3867a
409903113@qq.com <409903113@qq.com>
2015-09-30 11:34:34 +0800

    declare方法

--- inherited方法/_YellowDog.js
+++ inherited方法/_YellowDog.js
@@ -2,6 +2,7 @@ define([
     'declare',
     '_Dog'
 ],function(declare, _Dog){
+    //delcare方法 _Dog为YellowDog父类
     var YellowDog = declare([_Dog], {
         age: function(){
             //调用this.inherited方法，YellowDog实例，调用_Dog原型上的age方法
